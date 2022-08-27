12ed8f85aa45bd54df5e96c99644f8affc87a608
Antonio Martinez <martinezjenkins@gmail.com>
2022-08-27 23:03:18 +0200

    added fixture to test

--- tests/test_main.py
+++ tests/test_main.py
@@ -35,24 +35,30 @@ import math
 import sys
 import os
 
-game = Game()
-game.base = Base(15, 15)
-game.base.deployed = True
-game.base.gold = 100
-game.player = Player(10, 10)
-game.mines = []
-game.cannons = []
-game.mountains = [
-    Mountain(20, 20),
-]
-game.buildings = []
-game.satelites = []
-game.linterns = []
 
-game.bombs_activated = []
+@pytest.fixture
+def game():
+
+    game = Game()
+    game.base = Base(15, 15)
+    game.base.deployed = True
+    game.base.gold = 100
+    game.player = Player(10, 10)
+    game.mines = []
+    game.cannons = []
+    game.mountains = [
+        Mountain(20, 20),
+    ]
+    game.buildings = []
+    game.satelites = []
+    game.linterns = []
+
+    game.bombs_activated = []
+    return game
+
 
 class TestMine:
-    def test_build_ok(self):
+    def test_build_ok(self, game):
         """
         It is only possible to build a Mine in an empty space nearby a mountain
         (distance=1) but not on top of it, and with the influence of the base or
@@ -63,30 +69,26 @@ class TestMine:
         game.build_mine()
         assert len(game.mines) == 1
 
-    def test_no_build_on_mountain(self):
+    def test_no_build_on_mountain(self, game):
         game.player.y, game.player.x = (20, 20)
-        game.mines = []
         game.build_mine()
         assert len(game.mines) == 0
 
-    def test_no_build_far_from_mountain(self):
+    def test_no_build_far_from_mountain(self, game):
         game.player.y, game.player.x = (20, 18)
-        game.mines = []
         game.build_mine()
         assert len(game.mines) == 0
 
-    def test_no_build_far_from_base(self):
+    def test_no_build_far_from_base(self, game):
 
         game.mountains = [
             Mountain(2, 2),
         ]
         game.player = Player(2, 3)
-        game.mines = []
         game.build_mine()
         assert len(game.mines) == 0
 
-
-    def test_no_build_on_existing_building(self):
+    def test_no_build_on_existing_building(self, game):
         game.player.y, game.player.x = (20, 19)
         game.mines = [
             Mine(20, 19),
@@ -102,15 +104,13 @@ class TestMine:
         ## There should still be only 1 mine
         assert len(game.mines) == 1
 
-    def test_no_build_without_gold(self):
+    def test_no_build_without_gold(self, game):
         game.base.gold = 0
         game.player.y, game.player.x = (20, 19)
-        game.mines = []
         game.build_mine()
         assert len(game.mines) == 0
- 
 
-    def test_sell_ok(self):
+    def test_sell_ok(self, game):
         game.player.y, game.player.x = (20, 19)
         game.mines = [
             Mine(20, 19),
@@ -125,7 +125,7 @@ class TestMine:
         game.sell_building()
         assert len(game.mines) == 0
 
-    def test_no_sell_from_different_location(self):
+    def test_no_sell_from_different_location(self, game):
         game.player.y, game.player.x = (10, 10)
         game.mines = [
             Mine(10, 11),
@@ -142,7 +142,7 @@ class TestMine:
 
 
 class TestCannon:
-    def test_build_ok(self):
+    def test_build_ok(self, game):
         """
         It is only possible to build a Cannon in an empty space nearby a mountain
         (distance=1) but not on top of it, and with the influence of the base or
@@ -154,13 +154,12 @@ class TestCannon:
         game.build_cannon()
         assert len(game.cannons) == 1
 
-    def test_no_build_on_mountain(self):
+    def test_no_build_on_mountain(self, game):
         game.player.y, game.player.x = (20, 20)
-        game.cannons = []
         game.build_cannon()
         assert len(game.cannons) == 0
 
-    def test_no_build_on_existing_building(self):
+    def test_no_build_on_existing_building(self, game):
         game.player.y, game.player.x = (20, 19)
         game.cannons = [
             Cannon(20, 19),
@@ -176,20 +175,18 @@ class TestCannon:
         ## There should still be only 1 mine
         assert len(game.cannons) == 1
 
-    def test_no_build_far_from_base(self):
+    def test_no_build_far_from_base(self, game):
         game.player.y, game.player.x = (2, 3)
-        game.cannons = []
         game.build_cannon()
         assert len(game.cannons) == 0
 
-    def test_no_build_without_gold(self):
+    def test_no_build_without_gold(self, game):
         game.base.gold = 0
         game.player.y, game.player.x = (20, 19)
-        game.cannons = []
         game.build_cannon()
         assert len(game.cannons) == 0
 
-    def test_sell_ok(self):
+    def test_sell_ok(self, game):
         game.player.y, game.player.x = (20, 19)
         game.cannons = [
             Cannon(20, 19),
@@ -204,7 +201,7 @@ class TestCannon:
         game.sell_building()
         assert len(game.cannons) == 0
 
-    def test_no_sell_from_different_location(self):
+    def test_no_sell_from_different_location(self, game):
         game.player.y, game.player.x = (10, 10)
         game.cannons = [
             Cannon(10, 11),
@@ -221,34 +218,33 @@ class TestCannon:
 
 
 class TestLantern:
-    def test_build_lantern_ok(self):
+    def test_build_lantern_ok(self, game):
         game.base.gold = 100
         game.player.y, game.player.x = (20, 19)
-        game.linterns = []
         game.build_lantern()
         assert len(game.linterns) == 1
 
-    def test_no_build_lantern_without_gold(self):
+    def test_no_build_lantern_without_gold(self, game):
         game.base.gold = 0
         game.player.y, game.player.x = (20, 19)
-        game.linterns = []
         game.build_lantern()
         assert len(game.linterns) == 0
 
 
 class TestBombs:
-    def test_throw_bomb(self):
+    def test_throw_bomb(self, game):
         game.player.bombs = 1
         game.throw_bomb()
         assert game.player.bombs == 0
 
-    def test_no_throw_bomb_empty_inventory(self):
+    def test_no_throw_bomb_empty_inventory(self, game):
         game.player.bombs = 0
         game.throw_bomb()
         assert game.player.bombs == 0
 
+
 class TestBuildings:
-    def test_upgrade(self):
+    def test_upgrade(self, game):
         game.player.y, game.player.x = (20, 19)
 
         game.cannons = [
@@ -266,9 +262,9 @@ class TestBuildings:
         for lvl in range(1, 9):
             game.base.gold = building.cost_to_upgrade()
             game.upgrade_building()
-            assert building.level == lvl+1
+            assert building.level == lvl + 1
 
-    def test_no_upgrade_without_gold(self):
+    def test_no_upgrade_without_gold(self, game):
         game.player.y, game.player.x = (20, 19)
 
         game.cannons = [
@@ -287,4 +283,3 @@ class TestBuildings:
             game.base.gold = building.cost_to_upgrade() - 1
             game.upgrade_building()
             assert building.level == 1
- 
